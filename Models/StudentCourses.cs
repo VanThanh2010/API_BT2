cf18f5d8dc1421ac0f15e4e237b85910e7045cd0
VanThanh2010 <tavanthanh@gmail.com>
2024-04-03 17:31:36 +0700

    Add project files.

---
+++ Models/StudentCourses.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_BT2.Models
+{
+    public class StudentCourses
+    {
+        public Student? Student { get; set; }
+        public Guid? StudentId { get; set; }
+        public Courses? Courses { get; set; }
+        public Guid? CoursesId { get; set; }
+    }
+}
